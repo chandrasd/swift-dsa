1bae09013c25fd10d8d906f20f55e214a148194c
Chandrashekar Dasari <chandra.dasari.cs@gmail.com>
2025-07-28 01:40:27 -0700

    chore: renaming methods + new script for creating notes

---
+++ Scripts/create_new_note.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# TODO: Ability to create a new markdown file in /Notes
+
+create_new_note() {
+    local problems_dir = "/Notes"
+}
--- Scripts/create_new_problem.sh
+++ Scripts/create_new_problem.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # TODO: Ability to create a new swift file in /Problems and general structure needed to update the notes
 
-solve_new_problem() {
+create_new_problem() {
     local problems_dir = "/Problems"
 }
